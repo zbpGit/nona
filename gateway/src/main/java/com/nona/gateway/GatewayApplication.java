2e890314a2d8a30f2b335c64ae40174f7833a3cf
zbp <ideazbp@163.com>
2022-02-21 16:52:01 +0800

    gateway代码提交

---
+++ gateway/src/main/java/com/nona/gateway/GatewayApplication.java
@@ -0,0 +1,21 @@
+package com.nona.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Pierce
+ * <b>Description:</b><br>
+ * <b>ProjectName:nona</b>
+ * <br><b>PackageName:PACKAGE_NAME</b>
+ * <br><b>ClassName:GatewayApplication</b>
+ * <br><b>Date:2022/2/17 16:24</b>
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
---
+++ gateway/src/main/java/com/nona/gateway/config/JsonExceptionHandler.java
@@ -0,0 +1,97 @@
+package com.nona.gateway.config;
+
+import org.springframework.boot.autoconfigure.web.ErrorProperties;
+import org.springframework.boot.autoconfigure.web.ResourceProperties;
+import org.springframework.boot.autoconfigure.web.reactive.error.DefaultErrorWebExceptionHandler;
+import org.springframework.boot.web.reactive.error.ErrorAttributes;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.reactive.function.server.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Pierce
+ * <b>Description:自定义异常处理</b><br>
+ * <b>ProjectName:nona</b>
+ * <br><b>PackageName:com.nona.gateway.config</b>
+ * <br><b>ClassName:JsonExceptionHandler</b>
+ * <br><b>Date:2022/2/17 19:57</b>
+ */
+public class JsonExceptionHandler extends DefaultErrorWebExceptionHandler {
+
+    public JsonExceptionHandler(ErrorAttributes errorAttributes, ResourceProperties resourceProperties, ErrorProperties errorProperties, ApplicationContext applicationContext) {
+        super(errorAttributes, resourceProperties, errorProperties, applicationContext);
+    }
+
+    /**
+     * 构建返回的JSON数据格式
+     *
+     * @param status       状态码
+     * @param errorMessage 异常信息
+     * @return
+     */
+    public static Map<String, Object> response(int status, String errorMessage) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("status", status);
+        map.put("message", errorMessage);
+        map.put("data", null);
+        return map;
+    }
+
+    /**
+     * 获取异常属性
+     */
+    @Override
+    protected Map<String, Object> getErrorAttributes(ServerRequest request, boolean includeStackTrace) {
+        int code = 500;
+        Throwable error = super.getError(request);
+        if (error instanceof org.springframework.cloud.gateway.support.NotFoundException) {
+            code = 404;
+        }
+        return response(code, this.buildMessage(request, error));
+    }
+
+    /**
+     * 指定响应处理方法为JSON处理的方法
+     *
+     * @param errorAttributes
+     */
+    @Override
+    protected RouterFunction<ServerResponse> getRoutingFunction(ErrorAttributes errorAttributes) {
+        return RouterFunctions.route(RequestPredicates.all(), this::renderErrorResponse);
+    }
+
+    /**
+     * 根据code获取对应的HttpStatus
+     *
+     * @param errorAttributes
+     * @return
+     */
+    @Override
+    protected int getHttpStatus(Map<String, Object> errorAttributes) {
+        int statusCode = (int) errorAttributes.get("status");
+        return statusCode;
+    }
+
+    /**
+     * 构建异常信息
+     *
+     * @param request
+     * @param ex
+     * @return
+     */
+    private String buildMessage(ServerRequest request, Throwable ex) {
+        StringBuilder message = new StringBuilder("Failed to handle request [");
+        message.append(request.methodName());
+        message.append(" ");
+        message.append(request.uri());
+        message.append("]");
+        if (ex != null) {
+            message.append(": ");
+            message.append(ex.getMessage());
+        }
+        return message.toString();
+    }
+
+}
